a0474780de7d5713ad6cebb96761c20aff0e9a2c
Jo-Hakim <yoyohakim615@gmail.com>
2025-09-06 16:19:05 +0300

    commit final yes

--- script.js
+++ script.js
@@ -15,8 +15,30 @@ async function fetchArticles() {
 
 function showContent(id) {
   const sections = document.querySelectorAll(".page");
+  const theList = document.getElementById("unordered-list").children;
   sections.forEach((section) => {
     section.style.display = section.id === id ? "block" : "none";
+  });
+
+  Array.from(theList).forEach((anchor) => {
+    Array.from(anchor.children)[0].style.backgroundColor =
+      Array.from(theList).indexOf(anchor) ===
+      Array.from(sections).indexOf(
+        Array.from(sections).filter(
+          (section) => section.style.display == "block"
+        )[0]
+      )
+        ? "red"
+        : "white";
+    Array.from(anchor.children)[0].style.color =
+      Array.from(theList).indexOf(anchor) ===
+      Array.from(sections).indexOf(
+        Array.from(sections).filter(
+          (section) => section.style.display == "block"
+        )[0]
+      )
+        ? "white"
+        : "black";
   });
 
   const links = document.querySelectorAll("nav ul li a");
